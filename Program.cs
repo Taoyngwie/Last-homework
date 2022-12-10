48e6ce6a36a7bfb2ea91646899a1eddd2c3ec42d
taoyngwie <taoguitardz@gmil.com>
2022-12-10 15:08:55 +0700

    1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Tree<string> Tree = new Tree<string>();
+        Stack<string> Finding = new Stack<string>();
+        bool Met = false;
+
+        Tree.addingRootNode();
+        int countNum = 0;
+
+        do
+        {
+            string PeopleName = Console.ReadLine();
+            int Node = int.Parse(Console.ReadLine());
+            Tree.AddForTree(PeopleName, Node);
+            countNum++;
+
+        } while (Tree.GetLength() != countNum);
+
+        string vacationName = Console.ReadLine();
+        Tree.FindAns(Tree.getRoot(), vacationName, ref Finding, ref Met);
+
+
+    }
+}
